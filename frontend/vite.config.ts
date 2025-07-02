99483201661d9817c024af598a7100da790bcc98
unknown <121090811@link.cuhk.edu.cn>
2025-07-02 16:25:55 +0800

    same as last

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
- 
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    sourcemap: true
+  },
+  server: {
+    port: 5173,
+    strictPort: true
+  },
+  base: '/'
 }) 
